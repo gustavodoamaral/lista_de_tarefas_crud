5fb993208e4ceed13fd956a552ca1acea7cbbaef
Gustavo do Amaral <64814431+gustavodoamaral@users.noreply.github.com>
2021-07-16 17:11:42 -0300

    Consertado bug de Allowed Hosts

--- webdev/settings.py
+++ webdev/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^8wl=m=2z9f5-0sp*iwsv=gx*@zpu_(u=dn!_kry9vi4w-e6-l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
